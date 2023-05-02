eef082865af112f234e837acca29b8d3096c0a77
Martin <martinperoni95@gmail.com>
2023-05-02 22:44:33 +0200

    Ajout des dernières applications et modifications

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const Thing = require('./models/Thing');
+const path = require('path');
+const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauce');
 
 const app = express();
-
-app.use(express.json());
 
 mongoose.connect('mongodb+srv://mp95:i205gEyDjI3Gb40b@piquante.ubdissx.mongodb.net/test',
   { useNewUrlParser: true,
@@ -13,54 +12,18 @@ mongoose.connect('mongodb+srv://mp95:i205gEyDjI3Gb40b@piquante.ubdissx.mongodb.n
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-   // on indique que les ressources peuvent être partagées depuis n'importe quelle origine
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   // on indique les entêtes qui seront utilisées après la pré-vérification cross-origin afin de donner l'autorisation
-   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-   // on indique les méthodes autorisées pour les requêtes HTTP
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-   // on autorise ce serveur à fournir des scripts pour la page visitée
-   res.setHeader('Content-Security-Policy', "default-src 'self'");
-   next();
- });
-
- app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
+  app.use((req, res, next) => 
+  {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+      next();
   });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
 
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use(express.json());
 
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
-
-
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
