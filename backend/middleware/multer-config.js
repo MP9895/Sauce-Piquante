627d3e85c0c3bf4625a453fd7344eb6b117ce709
Martin <martinperoni95@gmail.com>
2023-05-31 23:07:38 +0200

    Changement sur la config de multer

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,20 +1,29 @@
 const multer = require('multer');
 
 const MIME_TYPES = {
-    'images/jpg': 'jpg',
-    'images/jpeg': 'jpg',
-    'images/png': 'png'
-}
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+    'image/webp': 'webp',
+};
 
 const storage = multer.diskStorage({
+
     destination: (req, file, callback) => {
         callback(null, 'images')
     },
+
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        let name = file.originalname.split(' ').join('-');
+        let newName = name.split('.');
+        newName.pop();
+        newName = newName.join('-');
+
         const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
-    } 
+        callback(null, newName + '_' + Date.now() + '.' + extension);
+    }
 })
 
-module.exports = multer({storage: storage}).single('image');
+const maxSize = 3 * 1024 * 1024;
+
+module.exports = multer({ storage: storage, limits: { fileSize: maxSize } }).single('image');
